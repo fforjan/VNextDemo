7f271a72bc0d812ce2937379304bca7b6ee45d66
Frederic Forjan <fforjan@free.fr>
2014-05-28 21:07:33 +0000

    Consume angularjs nuget package via redirection

--- source/Startup.cs
+++ source/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.FileSystems;
 using Microsoft.AspNet.StaticFiles;
+using Microsoft.AspNet.Http;
 
 namespace KWebStartup
 {
@@ -8,8 +9,15 @@ namespace KWebStartup
     {
         public void Configure(IBuilder app)
         {
-			// enables the rediretion of / to App folder
+			// rediretion of nuget packages
+			app.UseStaticFiles(new StaticFileOptions { 
+				RequestPath = new PathString("/Scripts/angular"),
+				FileSystem = new PhysicalFileSystem("../packages/angularjs.1.2.16/content/Scripts")});
+				
+			
+            // enables the rediretion of / to App folder			
 			app.UseStaticFiles(new StaticFileOptions { FileSystem = new PhysicalFileSystem("./App") });
+			
 			app.BuildTypeScript();
 		}
     }
